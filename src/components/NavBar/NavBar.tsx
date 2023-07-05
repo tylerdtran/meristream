9982bd0d31ef3d3658b2d69110f0a4a59f945fef
Tyler Tran <tylerduytran@gmail.com>
2023-07-05 14:51:17 -0700

    Changes to navbar saved

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -11,23 +11,37 @@ export default function NavBar() {
       setMobileMenuOpen(!isMobileMenuOpen);
     };  
 
+    // return (
+    //     <div className='navbar-total flex justify-between items-center w-full top-0 p-7'>
+    //         <div className="nav-title-holder">
+    //             <div className="nav-title uppercase">Meristream</div>
+    //         </div>
+    //         <div className="nav-menu">
+    //             <MenuIcon className={`hamburger ${isMobileMenuOpen ? 'open' : ''}`} onClick={toggleMobileMenu}></MenuIcon>
+    //             <div className="nav-element-holder flex flex-row gap-4 {`mobile-menu ${isMobileMenuOpen ? styles.open : ''}`}">
+    //                 <Link href="/devices"><div>Devices</div></Link>
+    //                 <Link href="/license"><div>License</div></Link>
+    //                 <Link href="/account"><div>Account</div></Link>
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
     return (
         <div className='navbar-total flex justify-between items-center w-full top-0 p-7'>
-            <div className="nav-title-holder">
-                <div className="nav-title uppercase">Meristream</div>
+          <div className="nav-title-holder">
+            <div className="nav-title uppercase">Meristream</div>
+          </div>
+          <div className="nav-menu">
+            <MenuIcon className={`hamburger ${isMobileMenuOpen ? 'open' : ''}`} onClick={toggleMobileMenu} />
+            <div className={`nav-element-holder flex flex-row gap-4 ${isMobileMenuOpen ? '' : 'hidden'}`}>
+              <Link href="/devices"><div>Devices</div></Link>
+              <Link href="/license"><div>License</div></Link>
+              <Link href="/account"><div>Account</div></Link>
             </div>
-            <div className="nav-menu">
-                <MenuIcon className={`hamburger ${isMobileMenuOpen ? 'open' : ''}`} onClick={toggleMobileMenu}></MenuIcon>
-                <div className={`mobile-menu nav-element-holder flex flex-row gap-4 ${isMobileMenuOpen ? 'open' : ''}`} >
-                    <Link href="/devices"><div>Devices</div></Link>
-                    <Link href="/license"><div>License</div></Link>
-                    <Link href="/account"><div>Account</div></Link>
-                </div>
-            </div>
+          </div>
         </div>
-    )
-
-      
+      );
+       
 }
 
 
